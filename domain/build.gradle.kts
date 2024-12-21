dde4acb4c2b32ad8637d49433398ed72d3049f66
Vafeen <666av6@gmail.com>
2024-12-22 00:00:14 +0300

    Update sdks

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.vafeen.concentration.domain"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.vafeen.concentration.resources"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
